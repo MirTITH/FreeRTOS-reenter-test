35770e390a5afc5dea9b748e412314fd52ae198c
TITH <1023515576@qq.com>
2022-09-06 10:09:49 +0800

    添加传入函数

--- UserCode/user_main.c
+++ UserCode/user_main.c
@@ -22,23 +22,25 @@ void StartDefaultTask(void const *argument)
     // 因为 malloc 等函数默认不可重入，不是线程安全的，而 sprintf, printf 等会调用这些函数
     // 开启那个宏定义后就可以使它们线程安全
 
+    float delta[8] = {0.1, 0.2, -0.1, -0.2, 1.5, -1, 0.5, -0.5};
+
     osThreadDef(Normal1, ThreadEntry, osPriorityNormal, 0, 128);
-    osThreadCreate(osThread(Normal1), NULL);
+    osThreadCreate(osThread(Normal1), &delta[0]);
     osThreadDef(Normal2, ThreadEntry, osPriorityNormal, 0, 128);
-    osThreadCreate(osThread(Normal2), NULL);
+    osThreadCreate(osThread(Normal2), &delta[1]);
     osThreadDef(Normal3, ThreadEntry, osPriorityNormal, 0, 128);
-    osThreadCreate(osThread(Normal3), NULL);
+    osThreadCreate(osThread(Normal3), &delta[2]);
     osThreadDef(Normal4, ThreadEntry, osPriorityNormal, 0, 128);
-    osThreadCreate(osThread(Normal4), NULL);
+    osThreadCreate(osThread(Normal4), &delta[3]);
 
     osThreadDef(ANormal1, ThreadEntry, osPriorityAboveNormal, 0, 128);
-    osThreadCreate(osThread(ANormal1), NULL);
+    osThreadCreate(osThread(ANormal1), &delta[4]);
     osThreadDef(ANormal2, ThreadEntry, osPriorityAboveNormal, 0, 128);
-    osThreadCreate(osThread(ANormal2), NULL);
+    osThreadCreate(osThread(ANormal2), &delta[5]);
     osThreadDef(ANormal3, ThreadEntry, osPriorityAboveNormal, 0, 128);
-    osThreadCreate(osThread(ANormal3), NULL);
+    osThreadCreate(osThread(ANormal3), &delta[6]);
     osThreadDef(ANormal4, ThreadEntry, osPriorityAboveNormal, 0, 128);
-    osThreadCreate(osThread(ANormal4), NULL);
+    osThreadCreate(osThread(ANormal4), &delta[7]);
     
     for (;;)
     {
